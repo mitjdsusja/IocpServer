ad0f303a7b60d2303ccfba3fd6d5e127a3d97621
mitjdsusja <mitjdsusja@naver.com>
2024-10-14 23:39:08 +0900

    CPU CORE THREAD

--- IocpServer/ServerMain.cpp
+++ IocpServer/ServerMain.cpp
@@ -1,11 +1,11 @@
 #include "pch.h"
 
+#include <windows.h>
+
 #include "ThreadManager.h"
 
 #include "Service.h"
 #include "PacketHandler.h"
-
-
 
 enum {
 	GQCS_THREAD_COUNT = 5,
@@ -13,6 +13,9 @@ enum {
 
 int main() {
 	wcout.imbue(std::locale("kor"));
+
+	SYSTEM_INFO sysInfo;
+	GetSystemInfo(&sysInfo);
 	
 	//ServerService* serverService = new ServerService(NetAddress(L"127.0.0.1", 7777), 100);
 	ServerService* serverService = new ServerService(NetAddress(L"192.168.0.14", 7777), 10);
@@ -20,7 +23,7 @@ int main() {
 
 	serverService->Start();
 	// Create Thread GQCS
-	for (int32 i = 0; i < GQCS_THREAD_COUNT; i++) {
+	for (int32 i = 0; i < sysInfo.dwNumberOfProcessors * 2; i++) {
 		cout << "Thread GQCS Start" << endl;
 
 		GThreadManager->Launch([=]() {
