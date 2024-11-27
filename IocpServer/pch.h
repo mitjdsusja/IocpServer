be2bacf9f6d2f349f57c08007937e3d563361da6
mitjdsusja <mitjdsusja@naver.com>
2024-11-28 00:40:05 +0900

    protobuf 21.0

--- IocpServer/pch.h
+++ IocpServer/pch.h
@@ -1,5 +1,6 @@
 #pragma once
 
 #pragma comment(lib,"ServerLib.lib")
+#pragma comment(lib,"libprotobufd.lib")
 
 #include "ServerPch.h"
--- protobuf/protobuf.bat
+++ protobuf/protobuf.bat
@@ -5,4 +5,10 @@ protoc.exe -I=./ --cpp_out=./ ./messageTest.proto
 XCOPY /Y messageTest.pb.cc "../ServerLib"
 XCOPY /Y messageTest.pb.h "../ServerLib"
 
+XCOPY /Y messageTest.pb.cc "../IocpServer"
+XCOPY /Y messageTest.pb.h "../IocpServer"
+
+XCOPY /Y messageTest.pb.cc "../IocpClient"
+XCOPY /Y messageTest.pb.h "../IocpClient"
+
 pause
