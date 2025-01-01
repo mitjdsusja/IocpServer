d86889dbe9d6e156cfad6eb420192e0b3bee78a7
mitjdsusja <mitjdsusja@naver.com>
2025-01-01 12:50:52 +0900

    packetHandler.h - remove before Serialization code

--- ServerLib/PacketHandler.h
+++ ServerLib/PacketHandler.h
@@ -28,61 +28,6 @@ struct PacketHeader {
 		return packetSize - sizeof(PacketHeader);
 	}
 };
-//
-//struct Packet_C_Request_User_Info{
-//
-//	int32 packetSize = 0;
-//	int32 packetId = PKT_CS_REQUEST_USER_INFO;
-//};
-//struct Packet_C_Request_Other_User_Info {
-//
-//	// TODO : 가변 데이터 처리
-//	int32 packetSize = 0;
-//	int32 packetId = 0;
-//};
-//struct Packet_C_Send_Pos {
-//	int32 packetSize = 0;
-//	int32 packetId = PKT_CS_SEND_POS;
-//
-//	int32 playerId = 0;
-//	float posX = 0;
-//	float posY = 0;
-//	float posZ = 0;
-//};
-//
-//struct Packet_S_Response_User_Info {
-//	int32 packetSize = 0;
-//	int32 packetId = PKT_SC_RESPONSE_USER_INFO;
-//
-//	int32 playerId = 0;
-//};
-//struct Packet_S_Response_Other_User_Info {
-//	int32 packetSize = 0;
-//	int32 packetId = PKT_SC_RESPONSE_OTHER_USER_INFO;
-//
-//	int32 playerCount = 0;
-//
-//	void AppendUserIdData(BYTE* packet, int32 userId) {
-//		packet += sizeof(int32) * (3 + playerCount);
-//		memcpy(packet, &userId, sizeof(int32));
-//		playerCount++;
-//	}
-//};
-//struct Packet_S_Broadcast_Pos {
-//	int32 packetSize = 0;
-//	int32 packetId = PKT_SC_BROADCAST_POS;
-//
-//	int32 playerId = 0;
-//	float posX = 0;
-//	float posY = 0;
-//	float posZ = 0;
-//};
-//struct Packet_S_Add_User {
-//	int32 packetSize = 0;
-//	int32 packetId = PKT_SC_ADD_USER;
-//
-//	int32 playerId = 0;
-//};
 #pragma pack(pop)
 
 static function<void(shared_ptr<Session> session, shared_ptr<Buffer> packet, Service* service)> packetHandleArray[UINT16_MAX];
