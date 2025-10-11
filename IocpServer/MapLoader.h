046866a27cc705554a4d67e3ca6f682709423709
mitjdsusja <mitjdsusja@naver.com>
2025-10-11 13:08:10 +0900

    MapLoader::LoadMap 구현 - 1

--- IocpServer/MapLoader.h
+++ IocpServer/MapLoader.h
@@ -1,8 +1,25 @@
 #pragma once
+#include <fstream>
 
+struct MapData {
+
+	int32 mapId;
+	int64 width;
+	int64 height;
+	vector<uint8> grid;
+};
 
 class MapLoader{
+public:
+	MapData LoadMap(const string& path) {
 
+		std::ifstream file(path, std::ios::binary);
+		MapData mapData;
+		if (!file) {
+			
+		}
 
+		file.read(reinterpret_cast<char*>(&mapData.mapId), sizeof(mapData.mapId));
+	}
 };
 
